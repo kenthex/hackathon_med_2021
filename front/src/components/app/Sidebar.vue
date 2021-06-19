2c70655c2972cdb70944b12af4efe0fc655899ba
konthex <lomaga.mail@gmail.com>
2021-06-19 22:04:45 +0300

    fixing7

---
+++ front/src/components/app/Sidebar.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul class="sidenav app-sidenav open" >
+
+      <li>
+        <a href="/" class="waves-effect waves-green pointer">Все товары</a>
+      </li>
+      <li>
+        <a href="/categories" class="waves-effect waves-green pointer">Категории</a>
+      </li>
+    </ul>
+</template>
+
+<script>
+export default {
+
+}
+</script>
